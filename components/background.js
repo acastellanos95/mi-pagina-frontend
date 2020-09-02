03de6b4db79ce2ddf890f706dad74e16da776df0
acastellanos95 <acastellanosaldama@gmail.com>
2020-09-02 10:47:54 -0500

    change tags

--- components/background.js
+++ components/background.js
@@ -3,13 +3,13 @@ import Styles from './background.module.css';
 export default function Background(){
   return(
     <div className={Styles.mainTextWrapper}>
-      <h3>
-        Background
-      </h3>
       <h5>
+        Background
+      </h5>
+      <p>
         Studied physics at BUAP where I learnt my first programming language (C++98). From there I began a journey seeking myself to get into some kind of software and programming development using physics.
         The first thing I developed was an LHC detector simulation from then I wrote programs about computational simulation theory where it all ended in my undergraduate thesis in molecular simulation. <i className="fas fa-rocket"></i>
-      </h5>
+      </p>
     </div>
   )
 }

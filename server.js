2d6d8960d43ad20c2db572581db283473a093c4e
acastellanos95 <acastellanosaldama@gmail.com>
2020-09-16 19:32:16 -0500

    added http and https server

---
+++ server.js
@@ -0,0 +1,27 @@
+const next = require("next");
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+
+const httpPort = 80;
+const httpsPort = 443;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const server = express();
+
+const options = {
+  key: fs.readFileSync(
+    "/etc/letsencrypt/live/andrecastellanos.dev/privkey.pem"
+  ),
+  cert: fs.readFileSync("/etc/letsencrypt/live/andrecastellanos.dev/cert.pem"),
+};
+
+app.prepare().then(() => {
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+  http.createServer(server).listen(httpPort);
+  https.createServer(options, server).listen(httpsPort);
+});

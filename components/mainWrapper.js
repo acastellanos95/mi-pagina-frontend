b4141d533815a512e75c9c3e8a2c4ec9a73154b7
acastellanos95 <acastellanosaldama@gmail.com>
2020-09-19 14:44:53 -0500

    css updated

--- components/mainWrapper.js
+++ components/mainWrapper.js
@@ -1,22 +1,45 @@
-import Styles from './mainImg.module.css';
+import Styles from "./mainImg.module.css";
 
-export default function MainWrapper(){
-  return(
+export default function MainWrapper() {
+  return (
     <div className={Styles.imagebg}>
-          <div className={Styles.wrapperText}>
-            <h3 className={Styles.mainTitle}>
-              André Castellanos
-            </h3>
-            <h5 className={Styles.mainDescription}>
-            Undergraduate Physicist and self-learning software engineer from México.
-            </h5>
-          </div>
-          <div className={Styles.BtnWrapper}>
-            <a href="https://www.facebook.com/andredevanymin" target="_blank" rel="noopener noreferrer" ><i className={"fab fa-facebook fa-lg "+Styles.BtnItem}></i></a>
-            <a href="https://wa.me/5212212851045" target="_blank" rel="noopener noreferrer" ><i className={"fab fa-whatsapp fa-lg "+Styles.BtnItem}></i></a>
-            <a href="https://github.com/acastellanos95" target="_blank" rel="noopener noreferrer" ><i className={"fab fa-github fa-lg "+Styles.BtnItem}></i></a>
-            <a href="https://www.linkedin.com/in/acastellanos95/" target="_blank" rel="noopener noreferrer" ><i className={"fab fa-linkedin-in fa-lg "+Styles.BtnItem}></i></a>
-          </div>
-        </div>
+      <div className={Styles.wrapperText}>
+        <h1>André Castellanos</h1>
+        <h2>
+          Undergraduate Physicist and self-learning software engineer from
+          México.
+        </h2>
+      </div>
+      <div className={Styles.BtnWrapper}>
+        <a
+          href="https://www.facebook.com/andredevanymin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={"fab fa-facebook fa-lg " + Styles.BtnItem}></i>
+        </a>
+        <a
+          href="https://wa.me/5212212851045"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={"fab fa-whatsapp fa-lg " + Styles.BtnItem}></i>
+        </a>
+        <a
+          href="https://github.com/acastellanos95"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={"fab fa-github fa-lg " + Styles.BtnItem}></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/acastellanos95/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={"fab fa-linkedin-in fa-lg " + Styles.BtnItem}></i>
+        </a>
+      </div>
+    </div>
   );
 }
